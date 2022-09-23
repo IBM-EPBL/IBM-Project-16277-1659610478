cfbb23b5faaef98718b5da664c85dccc30b245ce
sathyaomanduran <113964461+sathyaomanduran@users.noreply.github.com>
2022-09-23 15:05:05 +0530

    Add files via upload

---
+++ Assignment/Team Member 1/Assignment1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\msuba\OneDrive\Desktop\Assignment\Team Member 1\Assignment1\coding 1.py
+[25, 24, 23, 22, 21]
+[25, 24, 23, 22, 21, 35]
+[24, 23, 22, 21, 35]
+[24, 23, 22, 21, 35, 20]
+[20, 21, 22, 23, 24, 35]
+[20, 21, 22, 23, 24]
+[24, 23, 22, 21, 20]
+concatenatestring
+esrever
+ev
