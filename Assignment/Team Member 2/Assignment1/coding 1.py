e4af2a3d39e44903869ab8658b3acf7ef5828cd9
sathyaomanduran <113964461+sathyaomanduran@users.noreply.github.com>
2022-09-23 15:15:02 +0530

    Add files via upload

---
+++ Assignment/Team Member 2/Assignment1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[26,27,28,29,30]
+print(list)
+
+#insertion
+list.insert(5,40)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(25)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("string")
+str2=("operations")
+print(str1+str2)
+
+#reverse of String
+str3="string"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,4)])
+
+
+
+
+
+
+
+
+
+
+
+
