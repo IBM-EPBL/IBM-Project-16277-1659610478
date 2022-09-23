93c1cdf9b9b4f224f19d691477f0a1cb9df3bc7e
krithikakrishnamoorthy <113900593+krithikakrishnamoorthy@users.noreply.github.com>
2022-09-23 14:37:27 +0530

    Add files via upload

---
+++ Assignment/Team leader/Assignment1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\msuba\OneDrive\Desktop\Assignment\Team leader\Assignment1\Que 1\coding 1.py
+[21, 22, 23, 24, 25]
+[21, 22, 23, 24, 25, 30]
+[22, 23, 24, 25, 30]
+[22, 23, 24, 25, 30, 15]
+[15, 22, 23, 24, 25, 30]
+[15, 22, 23, 24, 25]
+[25, 24, 23, 22, 15]
+HelloWorld
+noitarepO
+per
---
+++ Assignment/Team leader/Assignment1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[21,22,23,24,25]
+print(list)
+
+#insertion
+list.insert(5,30)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(15)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Hello")
+str2=("World")
+print(str1+str2)
+
+#reverse of String
+str3="Operation"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,4)])
+
+
+
+
+
+
+
+
+
+
+
+
