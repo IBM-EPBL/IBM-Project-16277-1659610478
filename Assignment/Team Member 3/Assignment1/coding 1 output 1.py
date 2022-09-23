8f32aa02d1975eb1162ba0afba8de5a69506b470
sathyaomanduran <113964461+sathyaomanduran@users.noreply.github.com>
2022-09-23 15:38:13 +0530

    Add files via upload

---
+++ Assignment/Team Member 3/Assignment1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\msuba\OneDrive\Desktop\Assignment\Team Member 2\Assignment1\coding 1.py
+[31, 32, 33, 34, 35]
+[31, 32, 33, 34, 35, 40]
+[32, 33, 34, 35, 40]
+[32, 33, 34, 35, 40, 35]
+[32, 33, 34, 35, 35, 40]
+[32, 33, 34, 35, 35]
+[35, 35, 34, 33, 32]
+informationtechnology
+gnirts
+tr
---
+++ Assignment/Team Member 3/Assignment1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[31,32,33,34,35]
+print(list)
+
+#insertion
+list.insert(5,40)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(35)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("information")
+str2=("technology")
+print(str1+str2)
+
+#reverse of String
+str3="string"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,3)])
+
+
+
+
+
+
+
+
+
+
+
+
