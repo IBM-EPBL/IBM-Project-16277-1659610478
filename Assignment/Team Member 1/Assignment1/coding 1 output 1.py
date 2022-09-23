c703b0eb35eb1c4f734be078ad208ff35f1167c3
Renuzzz <113849525+Renuzzz@users.noreply.github.com>
2022-09-23 15:25:26 +0530

    Add files via upload

---
+++ Assignment/Team Member 1/Assignment1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\msuba\OneDrive\Desktop\Assignment\Team Member 1\Assignment1\coding 1.py
+[26, 27, 28, 29, 30]
+[26, 27, 28, 29, 30, 40]
+[27, 28, 29, 30, 40]
+[27, 28, 29, 30, 40, 25]
+[25, 27, 28, 29, 30, 40]
+[25, 27, 28, 29, 30]
+[30, 29, 28, 27, 25]
+stringoperations
+gnirts
+tri
